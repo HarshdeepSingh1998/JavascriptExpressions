bd894f3c3f36d01cf054a27d4c0ba73f12c07324
HarshdeepSingh1998 <88028787+HarshdeepSingh1998@users.noreply.github.com>
2021-08-09 18:41:22 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fname = "Angela";
+const lname = "Yu";
+const num = 7;
+
+ReactDOM.render(
+  <div>
+    {/* <h1>Hello {fname} {lname}!</h1> */}
+    <h1>Hello {`${fname}  ${lname}`}</h1>
+    {/* <p>Your lucky number is {num}</p> */}
+    <p>Your lucky number is {Math.floor(Math.random() * 10)}</p>
+  </div>,
+  document.getElementById("root")
+);
